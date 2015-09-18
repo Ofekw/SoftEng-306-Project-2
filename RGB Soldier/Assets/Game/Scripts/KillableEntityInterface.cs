b0dbfacb5b15da6a116803c054f87f0a6f7bdeca
Ofek <me@ofek.io>
2015-09-18 22:15:16 +1200

    Created base structures to be implemented by all entities. Movement implemented (but needs refining) Damage functions not implemented

---
+++ RGB Soldier/Assets/Game/Scripts/KillableEntityInterface.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class KillableEntityInterface : MonoBehaviour{
+    public int maxHealth = 3;
+    public int currentHealth = 3;
+    public bool dead = false;			// Whether or not the enemy is dead.
+
+
+    public abstract void takeDamage(int damageReceived);
+
+    public abstract void die();
+}
---
+++ RGB Soldier/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// Enforces these modules to be loaded up with this module when placed on a prefab/game object
+[RequireComponent(typeof(EntityMovement))]
+
+
+public class Player : KillableEntityInterface {
+
+    public EntityMovement entityMovement;
+
+	// Use this for initialization
+	void Start () 
+    {
+	 this.entityMovement = GetComponent<EntityMovement>();
+	}
+	
+	void FixedUpdate () 
+    {
+        //if pressing jump button, call jump method to toggle boolean
+        if (Input.GetButtonDown("Jump"))
+        {
+            entityMovement.Jump();
+        }
+        float hVelocity = Input.GetAxis("Horizontal");
+        //call the base movement module method to handle movement
+        entityMovement.Movement(hVelocity);
+	}
+
+    public override void takeDamage(int damageReceived)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void die()
+    {
+        throw new NotImplementedException();
+    }
+
+}
