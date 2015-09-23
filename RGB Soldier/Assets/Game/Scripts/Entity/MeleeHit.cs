a4c94d9505c26014276c9ce91773cfcc0b652703
amer236 <amer236@aucklanduni.ac.nz>
2015-09-23 16:26:35 +1200

    Added melee, by pressing control. Melee takes 0.1 seconds and hits anything directly in front of the player. It can be used every 0.3 seconds. Also added destructible crate to easily demonstrate melee. Changed Player script to use Update instead of fixed update, as it was missing some key inputs.

---
+++ RGB Soldier/Assets/Game/Scripts/Entity/MeleeHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeHit : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.gameObject.tag == "Crate" || hit.gameObject.tag == "Enemy")
+        {
+            hit.gameObject.SendMessage("takeDamage", 1);
+        }
+    }    
+}
