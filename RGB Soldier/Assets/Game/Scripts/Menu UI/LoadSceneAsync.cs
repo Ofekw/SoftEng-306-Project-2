0202a3128e7ebed18ed6e14c6d094dce6c261e15
lavitasy <christina.eir@gmail.com>
2015-09-23 21:30:42 +1200

    Added basic main menu screen with that can navigate to other screens (they only navigate to stage1).

---
+++ RGB Soldier/Assets/Game/Scripts/Menu UI/LoadSceneAsync.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadSceneAsync : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    // scene_id is given by the build settings on the project
+    public void ClickAsync(int scene_id)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(scene_id));
+    }
+
+    IEnumerator LoadLevelWithBar(int scene_id)
+    {
+        async = Application.LoadLevelAsync(scene_id);
+        // When scene is done loading in the background 
+        while (!async.isDone)
+        {
+            loadingBar.value = async.progress;
+            // Wait a frame before reevaluating this same expression
+            yield return null;
+        }
+    }
+}
