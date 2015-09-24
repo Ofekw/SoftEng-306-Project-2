af31e9cb908b03989cd2ee4b2fbd2f1865f71350
GetOffMyGit <hjoo488@aucklanduni.ac.nz>
2015-09-24 14:22:45 +1200

    Game state saves to a binary file in Unity's persistent data path.

--- RGB Soldier/Assets/Game/Scripts/Entity/GameControl.cs
+++ RGB Soldier/Assets/Game/Scripts/Entity/GameControl.cs
@@ -8,8 +8,15 @@ public class GameControl : MonoBehaviour {
 
     public static GameControl control;
 
-    public float playerHealth;
-    public float playerExperience;
+    public int playerLevel;
+    public int playerExp;
+    public int playerStr;
+    public int playerAgl;
+    public int playerDex;
+    public int playerInt;
+    public int playerVit;
+    public int currentGameLevel;
+
 
     //Save code on enable and disable if you want auto saving.
 
@@ -26,27 +33,69 @@ public class GameControl : MonoBehaviour {
         }
         
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void OnEnable()
+    {
+        Load();
+    }
+
+    void OnDisable()
+    {
+        Save();
+    }
 
     public void Save()
     {
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
 
+        PlayerData playerData = new PlayerData();
+
+        playerData.playerLevel = playerLevel;
+        playerData.playerExp = playerExp;
+        playerData.playerStr = playerStr;
+        playerData.playerAgl = playerAgl;
+        playerData.playerDex = playerDex;
+        playerData.playerInt = playerInt;
+        playerData.playerVit = playerVit;
+        playerData.currentGameLevel = currentGameLevel;
+
+        bf.Serialize(file, playerData);
+
+        file.Close();
     }
 
-    [Serializable]
-    class playerData
+    public void Load()
     {
-        //Implement getter and setters later.
-        public float playerHealth;
-        public float playerExperience;
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData) bf.Deserialize(file);
+            file.Close();
 
-
-
+            playerLevel = data.playerLevel;
+            playerExp = data.playerExp;
+            playerStr = data.playerStr;
+            playerAgl = data.playerAgl;
+            playerDex = data.playerDex;
+            playerInt = data.playerInt;
+            playerVit = data.playerVit;
+            currentGameLevel = data.currentGameLevel;
+        }
     }
+}
+
+[Serializable]
+class PlayerData
+{
+    //Implement getter and setters later.
+    public int playerLevel;
+    public int playerExp;
+    public int playerStr;
+    public int playerAgl;
+    public int playerDex;
+    public int playerInt;
+    public int playerVit;
+    public int currentGameLevel;
 }
