c68af8aaa3f20b268f8336f2e9ccae17a81276bb
unknown <LasharnNew@C12259-NS-2011.student.sk.edu>
2015-09-24 16:26:47 +1200

    Added pause button. Pause isn't bound to mouse button 1, not pause button. GameManager implementation begun

---
+++ RGB Soldier/Assets/Game/GameManager/GameManager.cs
@@ -0,0 +1,100 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class GameManager : MonoBehaviour
+//	where T : Component
+//{
+//	private static T _instance;
+//	public static T Instance {
+//		get {
+//			if (_instance == null) {
+//				_instance = FindObjectOfType<GameManager> ();
+//				if (_instance == null) {
+//					GameObject obj = new GameObject ();
+//					obj.hideFlags = HideFlags.HideAndDontSave;
+//					_instance = obj.AddComponent<T> ();
+//				}
+//			}
+//			return _instance;
+//		}
+//	}
+//
+//	public virtual void Awake() {
+//		DontDestroyOnLoad (this.gameObject);
+//		if (_instance == null) {
+//			_instance = this as T;
+//		} else {
+//			Destroy(gameObject);
+//		}
+//	}
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//}
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public static int orbsCollected = 0;
+	public static int specialAttackCounter = 0;
+	public static int enemiesOnScreen = 0;
+	public static int stage;
+	public const int ORB_COUNT_TARGET = 20;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject); //this enforces Singleton pattern
+		}
+		DontDestroyOnLoad (gameObject);
+		InitGame ();
+	}
+
+	void InitGame() {
+		orbsCollected = 0;
+		specialAttackCounter = 0;
+		enemiesOnScreen = 0;
+
+	}
+	void Update() {
+		Player player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player>();
+		if (player.currentHealth <= 0) {
+			gameOver();
+		}
+		if (orbsCollected >= ORB_COUNT_TARGET) {
+			levelCleared();
+		}
+	}
+
+	public static void incrementOrbCounter() {
+		orbsCollected++;
+	}
+
+	public static void incrementSpecialAtkCounter() {
+		specialAttackCounter++;
+	}
+
+	public static void resetSpecialAtkCounter() {
+		specialAttackCounter = 0;
+	}
+
+	void levelCleared ()
+	{
+		print ("Level has been cleared!");
+	}
+
+	void gameOver()
+	{
+		print ("You died lol");
+	}
+}
---
+++ RGB Soldier/Assets/PauseScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+	public bool paused;
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			paused = !paused;
+		}
+		if (paused) 
+		{
+			Time.timeScale = 0;
+		}
+		else if (!paused)
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
