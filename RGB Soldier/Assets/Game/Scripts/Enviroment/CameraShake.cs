72673c3779ae09d14e59ae3e8c21ce8658b8d551
Elizabeth Orchard <elizaorchard@gmail.com>
2015-09-27 10:39:31 +1300

    Added in camera shake

---
+++ RGB Soldier/Assets/Game/Scripts/Enviroment/CameraShake.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Enviroment
+{
+    class CameraShake : MonoBehaviour
+    {
+        // Transform of the camera to shake. Grabs the gameObject's transform
+        // if null.
+        public Transform camTransform;
+
+        // How long the object should shake for.
+        public float shake = 5f;
+
+        // Amplitude of the shake. A larger value shakes the camera harder.
+        public float shakeAmount = 0.7f;
+        public float decreaseFactor = 1.0f;
+
+        Vector3 originalPos;
+
+        void Awake()
+        {
+            if (camTransform == null)
+            {
+                camTransform = GetComponent(typeof(Transform)) as Transform;
+            }
+        }
+
+        void OnEnable()
+        {
+            originalPos = camTransform.localPosition;
+        }
+
+        void Update()
+        {
+            if (shake > 0)
+            {
+                camTransform.localPosition = originalPos + UnityEngine.Random.insideUnitSphere * shakeAmount;
+
+                shake -= Time.deltaTime * decreaseFactor;
+            }
+            else
+            {
+                shake = 0f;
+                camTransform.localPosition = originalPos;
+            }
+        }
+    }
+}
