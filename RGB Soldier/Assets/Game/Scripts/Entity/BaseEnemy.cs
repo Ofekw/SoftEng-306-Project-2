51b1be936f15790b1b32f0f8defb46b94cbae291
GetOffMyGit <hjoo488@aucklanduni.ac.nz>
2015-09-29 12:35:14 +1300

    Player experience system.

--- RGB Soldier/Assets/Game/Scripts/Entity/BaseEnemy.cs
+++ RGB Soldier/Assets/Game/Scripts/Entity/BaseEnemy.cs
@@ -9,6 +9,7 @@ public class BaseEnemy : KillableEntityInterface {
     public EntityMovement entityMovement;
     public int damageGiven = 1;
     public GameObject orb;
+    public int experienceGiven = 5;
 
     private Animator animator;                  //Used to store a reference to the Player's animator component.
 
@@ -69,6 +70,7 @@ public class BaseEnemy : KillableEntityInterface {
 
     public override void die()
     {
+        GameControl.control.giveExperience(experienceGiven);
         dead = true;
         Destroy(gameObject);
         if (Random.Range(0, 2) == 0)
---
+++ RGB Soldier/Assets/Game/Scripts/TemporaryCheckScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TemporaryCheckScript : MonoBehaviour {
+
+    public int playerLevel;
+    public int playerExp;
+    public int playerStr;
+    public int playerAgl;
+    public int playerDex;
+    public int playerInt;
+    public int playerVit;
+    public int currentGameLevel;
+    public int abilityPoints;
+
+    public Button theButton;
+
+
+	// Use this for initialization
+	void Start () {
+        playerLevel = GameControl.control.playerLevel;
+        playerExp = GameControl.control.playerExp;
+        playerStr = GameControl.control.playerStr;
+        playerAgl = GameControl.control.playerAgl;
+        playerDex = GameControl.control.playerDex;
+        playerInt = GameControl.control.playerInt;
+        playerVit = GameControl.control.playerVit;
+        abilityPoints = GameControl.control.abilityPoints;
+
+        theButton = GetComponent<Button>();
+        theButton.GetComponentInChildren<Text>().text = playerLevel.ToString() + " " + playerExp.ToString() + " " + abilityPoints.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void load()
+    {
+        GameControl.control.Save();
+        GameControl.control.Load();
+
+        playerLevel = GameControl.control.playerLevel;
+        playerExp = GameControl.control.playerExp;
+        playerStr = GameControl.control.playerStr;
+        playerAgl = GameControl.control.playerAgl;
+        playerDex = GameControl.control.playerDex;
+        playerInt = GameControl.control.playerInt;
+        playerVit = GameControl.control.playerVit;
+        abilityPoints = GameControl.control.abilityPoints;
+
+        theButton.GetComponentInChildren<Text>().text = playerLevel.ToString() + " " + playerExp.ToString() + " " + abilityPoints.ToString();
+    }
+}
