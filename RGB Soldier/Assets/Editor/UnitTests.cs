7134a32f767249077eff2537a04db6ed0acc20c4
GetOffMyGit <hjoo488@aucklanduni.ac.nz>
2015-10-05 15:20:37 +1300

    Added more unit tests.

--- RGB Soldier/Assets/Editor/UnitTests.cs
+++ RGB Soldier/Assets/Editor/UnitTests.cs
@@ -9,6 +9,9 @@ public class UnitTests
     public void jumpPressed()
     {
         Player sub = new Player();
+
+        Assert.True(!sub.isJumping);
+
         sub.jumpPressed();
 
 
@@ -16,17 +19,99 @@ public class UnitTests
     }
 
     [Test]
+    public void takeDamage()
+    {
+        Player player = new Player();
+        player.currentHealth = 5;
+
+        Assert.That(player.currentHealth == 5);
+
+        player.calculateDamage(1);
+
+        Assert.That(player.currentHealth == 4);
+    }
+
+    [Test]
     public void updateStats()
     {
         Player subPlayer = new Player();
         EntityMovement subEntityMovement = NSubstitute.Substitute.For<EntityMovement>();
+        GameControl subGameControl = new GameControl();
+        GameControl.control = subGameControl;
+
         subPlayer.entityMovement = subEntityMovement;
 
         subPlayer.vitality = 6;
         subPlayer.maxHealth = 3;
 
+        Assert.That(subPlayer.maxHealth == 3);
+
         subPlayer.UpdateStats();
 
         Assert.That(subPlayer.maxHealth == 6);
+    }
+
+    [Test]
+    public void levelUpCarryOver()
+    {
+        GameControl subGameControl = new GameControl();
+        subGameControl.playerLevel = 1;
+        subGameControl.playerExp = 100;
+        subGameControl.experienceRequired = 40;
+        subGameControl.checkExperience();
+
+        Assert.That(subGameControl.playerExp == 60);
+        Assert.That(subGameControl.playerLevel == 2);
+    }
+
+    [Test]
+    public void save() {
+        GameControl subGameControl = new GameControl();
+        PlayerData subPlayerData = new PlayerData();
+        subGameControl.playerData = subPlayerData;
+
+        subGameControl.playerStr = 8;
+        subGameControl.playerAgl = 7;
+        subGameControl.playerDex = 6;
+        subGameControl.playerInt = 5;
+        subGameControl.playerVit = 4;
+        subGameControl.currentGameLevel = 3;
+        subGameControl.abilityPoints = 2;
+
+        subGameControl.Save();
+
+        Assert.That(subPlayerData.playerStr == 8);
+        Assert.That(subPlayerData.playerAgl == 7);
+        Assert.That(subPlayerData.playerDex == 6);
+        Assert.That(subPlayerData.playerInt == 5);
+        Assert.That(subPlayerData.playerVit == 4);
+        Assert.That(subPlayerData.currentGameLevel == 3);
+        Assert.That(subPlayerData.abilityPoints == 2);
+    }
+
+    [Test]
+    public void load()
+    {
+        GameControl subGameControl = new GameControl();
+        PlayerData subPlayerData = new PlayerData();
+        subGameControl.playerData = subPlayerData;
+
+        subPlayerData.playerStr = 8;
+        subPlayerData.playerAgl = 7;
+        subPlayerData.playerDex = 6;
+        subPlayerData.playerInt = 5;
+        subPlayerData.playerVit = 4;
+        subPlayerData.currentGameLevel = 3;
+        subPlayerData.abilityPoints = 2;
+
+        subGameControl.Load();
+
+        Assert.That(subGameControl.playerStr == 8);
+        Assert.That(subGameControl.playerAgl == 7);
+        Assert.That(subGameControl.playerDex == 6);
+        Assert.That(subGameControl.playerInt == 5);
+        Assert.That(subGameControl.playerVit == 4);
+        Assert.That(subGameControl.currentGameLevel == 3);
+        Assert.That(subGameControl.abilityPoints == 2);
     }
 }
--- RGB Soldier/Assets/Game/Scripts/Entity/Player.cs
+++ RGB Soldier/Assets/Game/Scripts/Entity/Player.cs
@@ -86,28 +86,6 @@ public class Player : KillableEntityInterface
         {
             entityMovement.Jump();
         }
-        //float hVelocity = Input.GetAxis("Horizontal");
-        /*
-        float hVelocity = 0f;
-        if (moveRight && !moveLeft)
-        {
-            hVelocity = 1.0f;
-        }
-        else if (moveLeft && !moveRight)
-        {
-            hVelocity = -1.0f;
-        }
-        if (!moveRight && !moveLeft)
-        {
-            hVelocity = 0.0f;
-        }
-
-        if (hVelocity == 0)
-        {
-            hVelocity = Input.GetAxis("Horizontal");
-        }
-        */
-        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
         float hVelocity = CrossPlatformInputManager.GetAxis("Horizontal");
 
         if (hVelocity == 0)
@@ -215,26 +193,6 @@ public class Player : KillableEntityInterface
         }
     }
 
-    public void rightButtonPressed()
-    {
-        moveRight = true;
-    }
-
-    public void rightButtonReleased()
-    {
-        moveRight = false;
-    }
-
-    public void leftButtonPressed()
-    {
-        moveLeft = true;
-    }
-
-    public void leftButtonReleased()
-    {
-        moveLeft = false;
-    }
-
     public void jumpPressed()
     {
         isJumping = true;
@@ -251,15 +209,20 @@ public class Player : KillableEntityInterface
         if (!temporaryInvulnerable)
         {
             animator.SetTrigger("playerHit");
-            currentHealth--;
-            temporaryInvulnerable = true;
-            temporaryInvulnerableTime = Time.time;
-			print("You lost a life");
+            calculateDamage(damageReceived);
         }
         if (currentHealth <= 0)
         {
             die();
         }
+    }
+
+    public void calculateDamage(int damageReceived)
+    {
+        currentHealth--;
+        temporaryInvulnerable = true;
+        temporaryInvulnerableTime = Time.time;
+        print("You lost a life");
     }
 
     public override void die()
