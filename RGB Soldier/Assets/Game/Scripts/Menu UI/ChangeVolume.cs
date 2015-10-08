6acd909e55ffac767be7c0b99392ef69c055d6ac
Lavitasy <christina.eir@gmail.com>
2015-10-08 20:04:14 +1300

    Settings page added as a scene, basis functionality included

---
+++ RGB Soldier/Assets/Game/Scripts/Menu UI/ChangeVolume.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour {
+
+    public Text sliderText;
+    //get volume component
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void volumeUpdate(float textUpdateNumber)
+    {
+        string sliderTextString = ((int)textUpdateNumber).ToString();
+        sliderText.text = sliderTextString;
+
+        //TODO: change volume
+    }
+}
