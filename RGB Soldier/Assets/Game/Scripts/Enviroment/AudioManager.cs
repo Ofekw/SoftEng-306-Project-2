33423698fa0635082496f26f7e3c2493971e7a1d
Elizabeth Orchard <elizaorchard@gmail.com>
2015-10-11 12:39:59 +1300

    Updated the songs for the initial stages

--- RGB Soldier/Assets/Game/Scripts/Enviroment/AudioManager.cs
+++ RGB Soldier/Assets/Game/Scripts/Enviroment/AudioManager.cs
@@ -2,10 +2,120 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Game.Scripts.Enviroment
 {
-    class AudioManager
+    class AudioManager : MonoBehaviour
     {
+        private AudioSource audioSource;
+        private AudioClip lavaSong;
+        private AudioClip menuSong;
+        private AudioClip iceSong;
+        private AudioClip bossSong;
+        private AudioClip shrineSong;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            if (Application.loadedLevelName == "menu_start_screeen")
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            //loads in the audio tracks
+            menuSong = Resources.Load("Audio/menu_song") as AudioClip;
+            lavaSong = Resources.Load("Audio/lava_song") as AudioClip;
+            bossSong = Resources.Load("Audio/boss_battle_loop") as AudioClip;
+            iceSong = Resources.Load("Audio/ice_song") as AudioClip;
+            shrineSong = Resources.Load("Audio/Visager_-_03_-_Shrine") as AudioClip;
+            audioSource = GetComponent<AudioSource>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //audioSource.volume = AudioSettings.volumne;
+            if (Application.loadedLevelName == "menu_start_screen")
+            {
+                if (audioSource.clip != menuSong)
+                {
+                    audioSource.clip = menuSong;
+                }
+                //example of changing the music
+                audioSource.clip = menuSong;
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (Application.loadedLevelName == "menu_stage_select")
+            {
+                if (audioSource.clip != menuSong)
+                {
+                    audioSource.clip = menuSong;
+                }
+                //does not change the music
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (Application.loadedLevelName == "stage_1")
+            {
+                if (audioSource.clip != lavaSong)
+                {
+                    audioSource.clip = lavaSong;
+                }
+                //does not change the music
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (Application.loadedLevelName == "stage_2")
+            {
+                if (audioSource.clip != iceSong)
+                {
+                    audioSource.clip = iceSong;
+                }
+                //does not change the music
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (Application.loadedLevelName == "stage_boss")
+            {
+                audioSource.clip = bossSong;
+
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (Application.loadedLevelName.Contains("cutscene"))
+            {
+                audioSource.clip = shrineSong;
+
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        public void meleeAttackSound()
+        {
+        }
     }
 }
