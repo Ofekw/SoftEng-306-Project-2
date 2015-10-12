84e6a0f539c23d06a62258bf9b3c2b68c6adbc89
GetOffMyGit <hjoo488@aucklanduni.ac.nz>
2015-10-13 00:08:43 +1300

    Linked app to Google Play Services. Added Google's Play Service library. Linked one achievement.

---
+++ RGB Soldier/Assets/GoogleAuthenticate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+
+public class GoogleAuthenticate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) =>
+        {
+        });
+    }
+}
