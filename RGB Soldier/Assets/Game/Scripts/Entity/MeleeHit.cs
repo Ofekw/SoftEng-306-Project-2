43e1e71483bcf2710dd26412e5ee4e6c55d4a194
Patrick Poole <pat.stuart@hotmail.com>
2015-10-19 15:43:27 +1300

    Tweaked boss fight. Fixed shield down time bug. Changed melee damage dealt to boss to be based on strength.

--- RGB Soldier/Assets/Game/Scripts/Entity/MeleeHit.cs
+++ RGB Soldier/Assets/Game/Scripts/Entity/MeleeHit.cs
@@ -20,35 +20,15 @@ public class MeleeHit : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D hit)
     {
-        if (hit.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+        if (hit.gameObject.layer == LayerMask.NameToLayer("Enemies") || hit.gameObject.layer == LayerMask.NameToLayer("Boss"))
         {
 
             Instantiate(damageIndicator, hit.gameObject.transform.position, hit.gameObject.transform.rotation);
             SpriteRenderer renderer = GameObject.Find("numeric-1").GetComponent<SpriteRenderer>();
-            
+
             //Tell the meleed object that it has taken damage
             //Damage is equal to players strength
-            int meleeDmg = meleeMultiplier*player.GetComponent<Player>().strength;
-            hit.gameObject.SendMessage("takeDamage", meleeDmg );
-            if (meleeDmg < dmg.Length-1)
-            {
-                renderer.sprite = dmg[meleeDmg];
-            }
-            else
-            {
-                SpriteRenderer renderer2 = GameObject.Find("numeric-0b").GetComponent<SpriteRenderer>();
-                SpriteRenderer renderer3 = GameObject.Find("numeric-0a").GetComponent<SpriteRenderer>();
-                renderer.sprite = dmg[9];
-                renderer2.sprite = dmg[9];
-                renderer3.sprite = dmg[9];
-            }
-        }else if (hit.gameObject.layer == LayerMask.NameToLayer("Boss"))
-        {
-            Instantiate(damageIndicator, hit.gameObject.transform.position, hit.gameObject.transform.rotation);
-            SpriteRenderer renderer = GameObject.Find("numeric-1").GetComponent<SpriteRenderer>();
-
-            //static amount of damage dealt to boss
-            int meleeDmg = 2;
+            int meleeDmg = meleeMultiplier * player.GetComponent<Player>().strength;
             hit.gameObject.SendMessage("takeDamage", meleeDmg);
             if (meleeDmg < dmg.Length - 1)
             {
@@ -63,5 +43,5 @@ public class MeleeHit : MonoBehaviour {
                 renderer3.sprite = dmg[9];
             }
         }
-    }    
+    } 
 }
